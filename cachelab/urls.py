f8c8103b8311d394983447b2c70da8c59399aa90
Charles Reiss <creiss@virginia.edu>
2023-08-11 21:49:50 -0400

    support for reviewing old pattern questions

--- cachelab/urls.py
+++ cachelab/urls.py
@@ -8,6 +8,7 @@ from . import views
 urlpatterns = [
     path('', views.index_page, name='user-index'),
     path('pattern-question', views.last_pattern_question, name='last-pattern-question'),
+    path('pattern-question-list', views.list_pattern_questions , name='pattern-question-list'),
     path('pattern-question/<question_id>', views.pattern_question_detail, name='pattern-question'),
     path('submit-pattern-answer/<question_id>', views.pattern_answer, name='pattern-answer'),
     path('new-pattern-question', views.new_pattern_question, name='new-pattern-question'),
